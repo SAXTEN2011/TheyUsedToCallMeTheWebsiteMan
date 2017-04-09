b3dbdb70dfd1a8a85f5fe8500a064ada074e9ddd
Aaron <amaxpierce@yahoo.com>
2017-04-09 16:29:52 -0500

    added mobile version

---
+++ app.js
@@ -0,0 +1,29 @@
+const app = require('express')();
+var http = require('http').Server(app);
+
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+
+app.get('/', function (req, res) {
+    res.sendfile('./index.html');
+});
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '/')));
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+
+});
+
+module.exports = app;
